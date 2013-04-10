f40970c72e5e5371b49cedf1f23eec3586edfa8f
Lin Jen-Shin <godfat@godfat.org>
2013-04-10 19:24:53 +0800

    ignore payload

--- bench.sh
+++ bench.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+rm payload
 echo -n --b\r\n'Content-Disposition: form-data; name="f"; filename="payload"'\r\n\r\n > payload
 dd if=/dev/zero bs=5M count=1 >> payload
 echo -n \r\n--b--\r\n\r\n >> payload
